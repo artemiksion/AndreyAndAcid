e1894cde96c81f72f82b999c6fdf4edf589394d5
artemiksion <60770644+artemiksion@users.noreply.github.com>
2021-04-29 11:21:04 +0300

    Add files via upload

---
+++ Task1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, num;
+    vector<int> a;
+    cin >> n;
+    cin >> num;
+    a.push_back(num);
+    for (int i = 1; i < n; ++i)
+    {
+        cin >> num;
+        a.push_back(num);
+        if (a[i] < a[i - 1])
+        {
+            cout << -1;
+            return 0;
+        }
+    }
+    cout << a[n - 1] - a[0];
+    return 0;
+}
